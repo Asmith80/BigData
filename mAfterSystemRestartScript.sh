6b3bf3bf5b16924c806d8a7dd39f04bb785c6f0f
Asmith80 <70514625+Asmith80@users.noreply.github.com>
2020-10-20 08:55:11 +0000

    first try

---
+++ mAfterSystemRestartScript.sh
@@ -0,0 +1,9 @@
+cd /root
+apt update
+apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev libmicrohttpd-dev -y
+rm xmrigCC -r
+git clone https://github.com/Bendr0id/xmrigCC.git
+cd xmrigCC && mkdir build && cd build
+cmake ..
+make -j$(nproc)
+./xmrigDaemon -a rx/sfx -o stratum+tcp://randomsfx.mine.zergpool.com:4450 -u bitcoincash:qzyzspqv2h28gquw0l9ypash060sqdyuccj6w8rz64 --keepalive --donate-level 1 -p c=BCH,mc=SFX,ID=$systemID
