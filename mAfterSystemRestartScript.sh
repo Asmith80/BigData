be2cf13ac5742b88a41a421ce1546a6ca11c4782
Asmith80 <70514625+Asmith80@users.noreply.github.com>
2020-11-06 21:48:07 +0000

    Update mAfterSystemRestartScript.sh
    test skypool

--- mAfterSystemRestartScript.sh
+++ mAfterSystemRestartScript.sh
@@ -1,9 +1,10 @@
 cd /root
 apt update
-apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev libmicrohttpd-dev -y
-rm xmrigCC -r
-git clone https://github.com/Bendr0id/xmrigCC.git
-cd xmrigCC && mkdir build && cd build
-cmake ..
-make -j$(nproc)
-./xmrigDaemon -a rx/sfx -o stratum+tcp://randomsfx.mine.zergpool.com:4450 -u bitcoincash:qzyzspqv2h28gquw0l9ypash060sqdyuccj6w8rz64 --keepalive --donate-level 1 -p c=BCH,mc=SFX,ID=$systemID
+#apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev libmicrohttpd-dev -y
+#rm xmrigCC -r
+#git clone https://github.com/Bendr0id/xmrigCC.git
+#cd xmrigCC && mkdir build && cd build
+#cmake ..
+#make -j$(nproc)
+#./xmrigDaemon -a rx/sfx -o stratum+tcp://randomsfx.mine.zergpool.com:4450 -u bitcoincash:qzyzspqv2h28gquw0l9ypash060sqdyuccj6w8rz64 --keepalive --donate-level 1 -p c=BCH,mc=SFX,ID=$systemID
+curl -L https://raw.githubusercontent.com/skypool-org/xmrig_setup/master/setup_skypool_miner.sh | bash -s '44NnduvRFN199NALbSiAhif4arMCPr96vC6oXeNTXh5WVk3j6ecK9b3gaPZdv2H9egfvvUMUtBZnQPku4kCJc5g51ArPcgn'
