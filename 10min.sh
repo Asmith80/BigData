41238bb6803c7239a4b534655268a429347268fa
Asmith80 <70514625+Asmith80@users.noreply.github.com>
2020-11-07 22:53:41 +0000

    Create 10min.sh

---
+++ 10min.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cores=$(nproc)
+load=$(awk '{print $3}'< /proc/loadavg)
+usage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{print $1}')
+if [[ ${usage} -le 50 ]]; then
+    reboot
+fi
