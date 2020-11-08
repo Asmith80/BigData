663dbcc0dd65c8a719db00dafaf1d35abc6a5fc7
Asmith80 <70514625+Asmith80@users.noreply.github.com>
2020-11-08 01:54:35 +0000

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -3,5 +3,5 @@ cores=$(nproc)
 load=$(awk '{print $3}'< /proc/loadavg)
 usage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{print $1}')
 if [[ ${usage} -le 50 ]]; then
-    /sbin/shutdown -r +5
+    /sbin/shutdown -r now
 fi
